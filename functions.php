606ec45cd0107161e6e05188d5bd7d3aa3cad460
ninnypants <tyrel@ninnypants.com>
2012-07-01 12:09:52 -0600

    Added bare minimum of theme files and updater class

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+
+add_action('init', 'github_theme_updater_test_init');
+function github_theme_updater_test_init() {
+
+	include_once('updater.php');
+
+	define('WP_GITHUB_FORCE_UPDATE', true);
+
+	if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+
+		$config = array(
+			'proper_folder_name' => 'github-updater',
+			'api_url' => 'https://api.github.com/repos/ninnypants/WordPress-GitHub-Theme-Updater',
+			'raw_url' => 'https://raw.github.com/ninnypants/WordPress-GitHub-Theme-Updater/master',
+			'github_url' => 'https://github.com/ninnypants/WordPress-GitHub-Theme-Updater',
+			'zip_url' => 'https://github.com/ninnypants/WordPress-GitHub-Theme-Updater/zipball/master',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'readme.txt'
+		);
+
+		new WPGitHubThemeUpdater($config);
+
+	}
+
+}
